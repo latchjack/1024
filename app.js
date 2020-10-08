82f063846847245d5100c1d4feef8a2f25cfa4d5
Latch Jack <latch.jack@gmail.com>
2020-10-08 20:31:34 +0100

    remove class

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const gridDisplay = document.querySelector('.grid')
-  const scoreDisplay = document.queryElementById('.score')
-  const resultDisplay = document.queryElementById('.result')
-  const width = 4
+  const scoreDisplay = document.getElementById('score')
+  const resultDisplay = document.getElementById('result')
   let squares = []
+  const width = 4
+  let score = 0
 
   // create the board
   function createBoard() {
