2d616b464dda0a376f633d3e850b29b5c082227d
Latch Jack <latch.jack@gmail.com>
2020-10-09 14:40:17 +0100

    adding functions

--- app.js
+++ app.js
@@ -14,7 +14,111 @@ document.addEventListener('DOMContentLoaded', () => {
       gridDisplay.appendChild(square)
       squares.push(square)
     }
+    generate()
+    generate()
   }
   createBoard()
+
+  // generate a random number
+  function generate() {
+    randomNumber = Math.floor(Math.random() * squares.length)
+    if (squares[randomNumber].innerHTML == 0) {
+      squares[randomNumber].innerHTML = 2
+    } else generate()
+  }
+
+  // swipe right
+  function moveRight() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i + 1].innerHTML
+        let totalThree = squares[i + 2].innerHTML
+        let totalFour = squares[i + 3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = zeros.concat(filteredRow)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i + 1].innerHTML = newRow[1]
+        squares[i + 2].innerHTML = newRow[2]
+        squares[i + 3].innerHTML = newRow[3]
+      }
+    }
+  }
+  
+
+  // swipe left
+  function moveLeft() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i + 1].innerHTML
+        let totalThree = squares[i + 2].innerHTML
+        let totalFour = squares[i + 3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = filteredRow.concat(zeros)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i + 1].innerHTML = newRow[1]
+        squares[i + 2].innerHTML = newRow[2]
+        squares[i + 3].innerHTML = newRow[3]
+      }
+    }
+  }
+
+  // swipe down
+  function moveDown() {
+    for (let i = 0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML
+      let totalTwo = squares[i + width].innerHTML
+      let totalThree = squares[i + (width * 2)].innerHTML
+      let totalFour = squares[i + (width * 3)].innerHTML
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+      let filteredColumn = column.filter(num => num)
+      let missing = 4 - filteredColumn.length
+      let zeros = Array(missing)
+    }
+  }
+
+  function combineRow() {
+    for (let i = 0; i < 15; i++) {
+      if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML)
+        squares[i].innerHTML = combinedTotal
+        squares[i + 1].innerHTML = 0
+      }
+    }
+  }
+
+  // assign keycodes
+  function control(e) {
+    if (e.keyCode === 39) {
+      keyRight()
+    } else if (e.keyCode === 37) {
+      keyLeft()
+    }
+  }
+  document.addEventListener('keyup', control)
+
+  function keyRight() {
+    moveRight()
+    combineRow()
+    moveRight()
+    generate()
+  }
+
+  function keyLeft() {
+    moveLeft()
+    combineRow()
+    moveLeft()
+    generate()
+  }
 
 })
